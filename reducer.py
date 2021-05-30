ba417b20a6406617da5120bd471e44cc3647dd4e
niksnidds <nitish_acharya@seneca3.lehre.hwr-berlin.de>
2021-05-30 15:12:20 +0200

    first commit

--- reducer.py
+++ reducer.py
@@ -26,7 +26,6 @@ avg_of_values = 0
 # Previous key is initialized with None, we just started
 previous_key = None
 
-# For each new line in the standard input 
 for line in sys.stdin:
 
     # split the line at the tabulator ("\t")
@@ -48,18 +47,20 @@ for line in sys.stdin:
         # Line ends with new line (\n)
         sys.stdout.write("{0}\t{1}\n".format(previous_key, avg_of_values))
         # Sum of sales starts again with 0
-        sum_of_values = 0
         count_of_values = 0
-
+	sum_of_values = 0
+    
     # Add the value to the total sales
     # a += b is the same as a = a + b
     # the float function transforms the value
     # to a float data type (like decimal)
-    sum_of_values += float(value)
     count_of_values += 1
+    sum_of_values += float(value)
     avg_of_values = sum_of_values/count_of_values
+
     # the previous key for the next iteration is the current key of the this iteration 
     previous_key = key
-#if count_of_values > 114: 
+
 # write the last result to stdout
-    sys.stdout.write("{0}\t{1}\n".format(previous_key, avg_of_values))
+#if count_of_values > 114:  
+sys.stdout.write("{0}\t{1}\n".format(previous_key, avg_of_values))
