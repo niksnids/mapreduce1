20433afc2541c1845b997e8325e5f2349460af49
niksnidds <nitish_acharya@seneca3.lehre.hwr-berlin.de>
2021-05-30 12:54:03 +0200

    “[exercise2]”

--- reducer.py
+++ reducer.py
@@ -60,4 +60,4 @@ for line in sys.stdin:
     previous_key = key
 
 # write the last result to stdout
-sys.stdout.write("{0}\t{1}\n".format(previous_key, sum_of_values))
+sys.stdout.write("{0}\t{1}\n".format(previous_key, count_of_values))
